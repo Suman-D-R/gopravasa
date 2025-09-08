3deda8f34f9fac6266fd27fd5243154dbf8eae07
sumit8080 <sumit@pariltech.com>
2025-09-08 13:37:26 +0400

    Update Header component styles for improved visibility on scroll

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -43,7 +43,7 @@ export default function Header() {
   return (
     <header
       className={`fixed top-0 left-0 right-0 z-50  backdrop-blur-sm transition-colors duration-00 ${
-        isScrolled ? ' text-gray-800' : 'bg-transparent text-white'
+        isScrolled ? ' text-gray-800 bg-white/50' : 'bg-transparent text-white'
       }`}
       ref={menuRef}
     >
